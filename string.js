9509af71406a849ec94d258a2eb693438a94650c
Alex-Githinji <90707315+Alex-Githinji@users.noreply.github.com>
2024-03-13 20:33:09 +0300

    string.js

--- string.js
+++ string.js
@@ -1,3 +1,7 @@
+// Write a program that accepts a string as input, capitalizes the first letter of each word in the
+string, and then returns the result string.
+
+
 function capitalizeFirstLetter(str) {
 
     let words = str.split(' ');
