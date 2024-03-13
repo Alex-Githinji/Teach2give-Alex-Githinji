b8eab4ceed56d3d9a01eaa0e7a2eb4ab82e13fe4
Alex-Githinji <90707315+Alex-Githinji@users.noreply.github.com>
2024-03-13 18:05:09 +0300

    Add files via upload
    Write a program to generate the Fibonacci sequence up to 100.

---
+++ Fibonancci.py
@@ -0,0 +1,24 @@
+def fibonacci(limit):
+    
+    fib_sequence = [0, 1]
+    
+   
+    while True:
+       
+        next_fib = fib_sequence[-1] + fib_sequence[-2]
+        
+       
+        if next_fib > limit:
+            break
+        
+        
+        fib_sequence.append(next_fib)
+    
+    return fib_sequence
+
+
+fib_sequence = fibonacci(100)
+
+
+print("Fibonacci sequence up to 100:")
+print(fib_sequence)
