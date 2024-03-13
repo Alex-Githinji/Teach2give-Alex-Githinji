b70082d70f157d5e09bc46d6154178e2c0a42ef6
Alex-Githinji <90707315+Alex-Githinji@users.noreply.github.com>
2024-03-13 17:58:19 +0300

    FizzBuzz.py

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -1,3 +1,8 @@
+# Write a program that prints the numbers from 1 to 100. For multiples of 3, print "Fizz"; for
+multiples of 5, print "Buzz"; and for numbers that are multiples of both 3 and 5, print
+"FizzBuzz".
+
+
 for num in range(1, 101):
     if num % 3 == 0 and num % 5 == 0:
         print("FizzBuzz")
